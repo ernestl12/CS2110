5a8b7a4224c9d7d9219fdd1e5b6d2bf7f95941a5
Edwin Young <edwin@cornell>
2012-09-09 16:22:18 -0400

    Removed testing code

--- HW 1/cs2110/ewy7/assignment1/RadioRecommenderSystem.java
+++ HW 1/cs2110/ewy7/assignment1/RadioRecommenderSystem.java
@@ -26,8 +26,7 @@ public class RadioRecommenderSystem {
    * @param args The first argument should contain the folder path for the three files. 
    */
   public static void main(String[] args) {
-//    String filepath = args[0];
-    String filepath = ".";
+    String filepath = args[0];
     
     BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
     
