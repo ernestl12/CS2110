b2cb7a7154f4d2a155dbf4293ed91507943fef76
ernestl12 <ecl85@cornell.edu>
2012-10-04 00:01:10 -0300

    Update HW 2/cs2110/ewy7/assignment2/ScheduleGenerator.java

--- HW 2/cs2110/ewy7/assignment2/ScheduleGenerator.java
+++ HW 2/cs2110/ewy7/assignment2/ScheduleGenerator.java
@@ -14,7 +14,9 @@ import java.util.Arrays; // you can only use toString & sort
 public class ScheduleGenerator {
 	private int[] tasks;
 	private int m;
-	private int[] _mValues;
+	private int optimalMakespan = 9999;
+	private ScheduledTask[] optimalST;
+	private int count = 0;
 
 	/**
 	 * If the arguments are not valid, throw an IllegalArgumentException.
@@ -64,10 +66,107 @@ public class ScheduleGenerator {
 	 * Find an optimal schedule, that is one with minimum makespan.
 	 */
 	public Schedule getOptSchedule() {
-		// TODO: Implement me.
-		return null;
+		
+		int[] mValues = new int[m];
+		
+		//tempTasks is a replica of tasks
+		//used to keep track of which tasks have been exhausted
+		//true = available, false = used
+		boolean[] tempTasks = new boolean[tasks.length];
+		for(int index = 0; index < tasks.length; index++) {
+			tempTasks[index] = true;
+		}
+		
+		//first task will always be assigned to processor 0
+		ScheduledTask[] st = new ScheduledTask[tasks.length];
+		st[0] = new ScheduledTask(0 ,0);
+		tempTasks[0] = false;
+		mValues[0] += tasks[0];
+		
+		Schedule best = new Schedule(tasks, m, optimize(tempTasks, mValues, st, 1));
+		System.out.println("Trials: " + count);
+		return best;
+	}
+
+	//recursive method parameters: remaining keeps track of which tasks have been used up
+	//current keeps track of current values of each processor
+	//temp keeps track of the current ScheduledTask[] array
+	//NOTE: The first task is the control value. It doesn't change with recursion to reduce repetition.
+	private ScheduledTask[] optimize(boolean[] taskList, int[] values, ScheduledTask[] schedtask, int index) {
+		int[] current = values;
+		boolean[] remaining = taskList;
+		ScheduledTask[] temp = schedtask;
+		
+		//if all tasks are used, return the full schedule of tasks		
+		if(used(remaining)) {
+			return temp;
+		}
+		
+		if(remaining[index]) {
+			for(int processor = 0; processor < m; processor++) {
+				current[processor] += tasks[index];
+
+				System.out.println("temp index: " + index);
+				System.out.println(temp[index]);
+				
+				if(temp[index] == null) {
+					temp[index] = new ScheduledTask(index, processor);
+				}
+				else {
+					temp[index].p = processor;
+				}
+				System.out.println("temp index: " + index);
+				System.out.println(temp[index]);
+				remaining[index] = false;
+					
+				System.out.println("JUMPING");
+				temp = optimize(remaining, current, temp, index + 1);
+				System.out.println("RETURNED");
+
+				int makespan = obtainMakespan(temp);
+				
+				if(used(remaining) && makespan < optimalMakespan) {
+					optimalMakespan = makespan;
+					optimalST = temp;
+					System.out.println("New Optimal: " + optimalMakespan);
+				}
+				else {
+					current[processor] -= tasks[index];
+					remaining[index] = true;
+				}
+			}
+		}
+		return optimalST;
+	}
+
+	//checks to see whether all tasks are used
+	private boolean used(boolean[] checkList) {
+		for(int p = 0; p < checkList.length; p++)
+		{
+			if(checkList[p]) {
+				System.out.println("Checking task usage...false!");
+				return false;
+			}
+		}
+		System.out.println("Checking task usage...true!");
+		return true;
 	}
 	
+	//self explanatory
+	private int obtainMakespan(ScheduledTask[] st) {
+		for(int i = 0; i < st.length; i++) {
+			System.out.println(st[i]);
+		}
+		System.out.println("Schedule creation");
+		Schedule sched = new Schedule(tasks, m, st);
+		for(int i = 0; i < st.length; i++) {
+			System.out.println(st[i]);
+		}
+		System.out.println(sched);
+		count++;
+		return sched.getMakespan();
+	}	
+
 	/**
 	 * NOTE: You do NOT have to use this class if you don't want to or
 	 * you can't see why it's useful.
